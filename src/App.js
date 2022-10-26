29cba002cb0a0ee5ebdcb5fa6ac02dbbaa5d125e
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-26 13:47:43 +0100

    Changes committed: 	modified:   src/App.js 	renamed:    src/components/Footer.js -> src/components/Footer/Footer.js 	deleted:    src/components/Header.js 	new file:   src/components/Header/Header.js  Files Created: 	src/components/Footer/Footer.css 	src/components/Header/Header.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import List from "./components/List";
+import Footer from "./components/List";
 const App = () => {
   return <div>App</div>;
 };
