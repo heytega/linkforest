434bd5b358adfb8fc66c1101a318ce468334c7ac
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-26 15:00:14 +0100

    modified:   src/components/Header/Header.js 	renamed:    images/profile__img.png -> src/images/profile__img.png

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Header.css";
+import Profile_image from "../../images/profile__img.png";
 
 const Header = () => {
   return (
     <div className="header-container">
       <div className="profile-container">
-        <img src="" alt="" id="profile_img" />
+        <img src={Profile_image} alt="profile image" id="profile_img" />
+        <h1>ChristianTae</h1>
       </div>
     </div>
   );
