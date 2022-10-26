43848f5766abacb105ddcfd4aad696beacb26bd9
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-26 14:48:21 +0100

    new file:   src/data.js

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const data = [
+  {
+    tag: "Twitter Link",
+    id: "twitter",
+    link: "https://www.twitter.com/TegaDev",
+  },
+  {
+    tag: "Zuri Team",
+    id: "btn_zuri",
+    link: "https://training.zuri.team",
+  },
+  {
+    tag: "Zuri Books",
+    id: "books",
+    link: "http://books.zuri.team",
+  },
+  {
+    tag: "Python Books",
+    id: "book_python",
+    link: "https//books.zuri.team/python-for-beginners?ref_id=${slackName}",
+  },
+  {
+    tag: "Background Check for Coders",
+    id: "pitch",
+    link: "https//background.zuri.team",
+  },
+  {
+    tag: "Design Books",
+    id: "book_Design",
+    link: "https://books.zuri.team/design-rules",
+  },
+];
