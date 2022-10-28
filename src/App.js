537ac29faf8499b9816e5d1c3d5edbe2752d2171
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-10-28 15:46:03 +0100

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "./components/header/Header";
 import List from "./components/list/List";
-import Footer from "./components/list/List";
+import Footer from "./components/footer/Footer";
 import "./App.css";
 
 const App = () => {
   return (
-    <>
+    <div className="main-container">
       <Header />
       <List />
       <Footer />
-    </>
+    </div>
   );
 };
 
