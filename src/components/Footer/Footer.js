2e64ecd320e987a30efd4541488645d3f4ef8097
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-01 15:20:55 +0100

    new file:   src/Context.js 	modified:   src/components/Footer/Footer.css 	modified:   src/components/Footer/Footer.js 	modified:   src/index.css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Footer.css";
+import ZuriLogo from "../../images/zuri_logo.svg";
+import I4gLogo from "../../images/4G.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <a href="">
+        <img src={ZuriLogo} alt="Zuri Logo" />
+      </a>
+      <h2>HNG Internship 9 Frontend Task</h2>
+      <a href="">
+        <img src={I4gLogo} alt="I4G Logo" />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
