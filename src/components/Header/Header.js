1a3f4780365577f7ef1d9d2e1f97ae01c7961c02
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-01 09:49:21 +0100

    modified:   src/components/Header/Header.js 	modified:   src/components/list/List.css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import Profile_image from "../../images/profile__img.png";
+import Profile_img from "../../images/profile__img.png";
 
 const Header = () => {
+  const [twitter, setTwitter] = useState({
+    id: "twitter",
+    username: "Annette Black",
+  });
+
+  const [slack, setSlack] = useState({
+    id: "slack",
+    username: "Tega Akpovbiyor",
+  });
+
   return (
     <div className="header-container">
-      <div className="profile-container">
-        <img src={Profile_image} alt="profile image" id="profile_img" />
-        <h1>ChristianTae</h1>
+      <div className="profile-section">
+        <img src={Profile_img} alt="Profile display" id="profile_img" />
+        <h1 id={twitter.id}>{twitter.username}</h1>
+        <h1 id={slack.id}>{slack.username}</h1>
       </div>
     </div>
   );
