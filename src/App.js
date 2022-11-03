cc1abbe6b361d2ded1b9e693b93ad0979454bddb
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-03 12:13:08 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Components/Header/Header";
 import List from "./Components/List/List";
 import Footer from "./Components/Footer/Footer";
@@ -6,15 +6,21 @@ import Socials from "./Components/Socials/Socials";
 import "./App.css";
 
 const App = () => {
-  const [twitter, setTwitter] = useState({
-    id: "twitter",
-    username: "ChristianTAE",
-  });
+  const [twitter, setTwitter] = useState({});
 
-  const [slack, setSlack] = useState({
-    id: "slack",
-    username: "Tega Akpovbiyor",
-  });
+  const [slack, setSlack] = useState({});
+
+  useEffect(() => {
+    setTwitter({
+      id: "twitter",
+      username: "ChristianTAE",
+    });
+
+    setSlack({
+      id: "slack",
+      username: "Tega Akpovbiyor",
+    });
+  }, []);
 
   return (
     <div className="main-container">
