d5dfc041efe20edc82e24bf165fc1ddc059a7682
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-03 15:22:38 +0100

    Refactored the App.js to accomdate for BrowserRoutes to ContactPage. Location of modification:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Header from "./component_folder/header_folder/Header";
-import List from "./component_folder/list_folder/List";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import ContactPage from "./ContactPage";
 import Footer from "./component_folder/footer_folder/Footer";
-import Socials from "./component_folder/socials_folder/Socials";
 import "./App.css";
 
 const App = () => {
-  const [twitter, setTwitter] = useState({});
-
-  const [slack, setSlack] = useState({});
-
-  useEffect(() => {
-    setTwitter({
-      id: "twitter",
-      username: "ChristianTAE",
-    });
-
-    setSlack({
-      id: "slack",
-      username: "Tega Akpovbiyor",
-    });
-  }, []);
-
   return (
     <div className="main-container">
-      <Header twitter={twitter} slack={slack} />
-      <List slack={slack} />
-      <Socials />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/Contact" element={<ContactPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
