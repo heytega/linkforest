87f64d110cb03bc7731c9cc635cb50d6afaa2020
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-04 10:19:33 +0100

    refactor: created states passed them down to components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import ContactPage from "./ContactPage";
+import Home from "./route_folder/home_page/Home";
+import ContactPage from "./route_folder/contact_page/ContactPage";
 import Footer from "./component_folder/footer_folder/Footer";
 import "./App.css";
 
 const App = () => {
+  const [twitter, setTwitter] = useState({});
+
+  const [slack, setSlack] = useState({});
+
+  useEffect(() => {
+    setTwitter({
+      id: "twitter",
+      username: "ChristianTAE",
+    });
+
+    setSlack({
+      id: "slack",
+      username: "Tega Akpovbiyor",
+    });
+  }, []);
   return (
     <div className="main-container">
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/Contact" element={<ContactPage />} />
+          <Route
+            exact
+            path="/"
+            element={<Home twitter={twitter} slack={slack} />}
+          />
+          <Route path="/Contact" element={<ContactPage twitter={twitter} />} />
         </Routes>
         <Footer />
       </BrowserRouter>
