f4295cca93b7631bd405489a26a4f07032396377
Tega Akpovbiyor <akpovbiyoranthony@gmail.com>
2022-11-04 20:21:55 +0100

    styling: styled the form on the contact page

--- src/component_folder/form_folder/Form.js
+++ src/component_folder/form_folder/Form.js
@@ -3,59 +3,71 @@ import "./Form.css";
 
 const Form = ({ twitter }) => {
   const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <form>
       <div className="form-flex-container">
         <div className="form-control">
-          <label htmlFor="firstName">Name </label>
+          <label htmlFor="firstName">First name</label>
           <input
             type="text"
             id="first_name"
             name="firstName"
+            placeholder="Enter your first name"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
           />
         </div>
         <div className="form-control">
-          <label htmlFor="firstName">Name : </label>
+          <label htmlFor="firstName">Last name</label>
           <input
             type="text"
             id="last_name"
             name="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="Enter your last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
           />
         </div>
       </div>
-
       <div className="form-control">
-        <label htmlFor="email">Name : </label>
+        <label htmlFor="email">Email</label>
         <input
           type="text"
           id="email"
           name="email"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="Yourname@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+
       <div className="form-control">
-        <label htmlFor="message">Message : </label>
+        <label htmlFor="message">Message</label>
         <textarea
           name="message"
           id="message"
-          cols="30"
-          rows="10"
+          //   cols="30"
+          //   rows="10"
           placeholder="Send me a message and I'll reply as soon as possible..."
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
         ></textarea>
       </div>
-      <div className="checkbox">
-        <input type="checkbox" name="checkbox" />
-        <label htmlFor="checkbox">
+      <div className="form-control">
+        <label className="checkbox-container">
           You agree to providing your data to {twitter.username} who may contact
           you
+          <input type="checkbox" />
+          <span className="checkmark"></span>
         </label>
+      </div>
+      <div className="form-control">
+        {/* <input className="btn" value="Send message" /> */}
+        <button className="btn">Send message</button>
       </div>
     </form>
   );
